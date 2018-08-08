f7a46d37a25798df7cda237251d5ef38b36ce71a
cuongdevpro <cuongdevpro@gmail.com>
2018-08-08 23:15:10 +0700

    Structure client js

---
+++ ERPCartonWeb/ERPCartonWeb/wwwroot/app/shared/cuong.js
@@ -0,0 +1,152 @@
+﻿var cuong = {
+    configs: {
+        pageSize: 10,
+        pageIndex: 1
+    },
+    notify: function (message, type) {
+        $.pnotify(message, {
+            title: 'Animate.css Effect',
+            text: message,
+            type: type,
+            animate: {
+                animate: true,
+                in_class: 'zoomInLeft',
+                out_class: 'zoomOutRight'
+            }
+        })
+    },
+    confirm: function (message, okCallback) {
+        $.pnotify.confirm({
+            text: message,
+            icon: 'glyphicon glyphicon-question-sign',
+            hide: false,
+
+            confirm: {
+                confirm: true,
+                buttons: [{
+                    text: 'Dong Y',
+                    addClass: 'btn-primary',
+                    click: function (notice) {
+                        notice.update({
+                            title: 'Dong Y',
+                            text: 'Yeu Cau cua ban duoc chap nhan',
+                            icon: true,
+                            type: 'success',
+                            hide: true,
+                            confirm: {
+                                confirm: false
+                            },
+                            buttons: {
+                                closer: true,
+                                sticker: true
+                            },
+                            okCallback
+                        });
+                    }
+                }, {
+                    text: 'Huy',
+                    click: function (notice) {
+                        notice.update({
+                            title: 'Huy',
+                            text: 'He thong huy yeu cau cua ban',
+                            icon: true,
+                            type: 'error',
+                            hide: true,
+                            confirm: {
+                                confirm: false
+                            },
+                            buttons: {
+                                closer: true,
+                                sticker: true
+                            }
+                        });
+                    }
+                }]
+            },
+            history: {
+                history: false
+            }
+        })
+    },
+
+    dateFormatJson: function (datetime) {
+        if (datetime == null || datetime == '')
+            return '';
+        var newdate = new Date(parseInt(datetime.substr(6)));
+        var month = newdate.getMonth() + 1;
+        var day = newdate.getDate();
+        var year = newdate.getFullYear();
+        var hh = newdate.getHours();
+        var mm = newdate.getMinutes();
+        if (month < 10)
+            month = "0" + month;
+        if (day < 10)
+            day = "0" + day;
+        if (hh < 10)
+            hh = "0" + hh;
+        if (mm < 10)
+            mm = "0" + mm;
+        return day + "/" + month + "/" + year;
+    },
+    dateTimeFormatJson: function (datetime) {
+        if (datetime == null || datetime == '')
+            return '';
+        var newdate = new Date(parseInt(datetime.substr(6)));
+        var month = newdate.getMonth() + 1;
+        var day = newdate.getDate();
+        var year = newdate.getFullYear();
+        var hh = newdate.getHours();
+        var mm = newdate.getMinutes();
+        var ss = newdate.getSeconds();
+        if (month < 10)
+            month = "0" + month;
+        if (day < 10)
+            day = "0" + day;
+        if (hh < 10)
+            hh = "0" + hh;
+        if (mm < 10)
+            mm = "0" + mm;
+        if (ss < 10)
+            ss = "0" + ss;
+        return day + "/" + month + "/" + year + " " + hh + ":" + mm + ":" + ss;
+    },
+    startLoading: function () {
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading').removeClass('hide');
+    },
+    stopLoading: function () {
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading')
+                .addClass('hide');
+    },
+    getStatus: function (status) {
+        if (status == 1)
+            return '<span class="badge bg-green">Kích hoạt</span>';
+        else
+            return '<span class="badge bg-red">Khoá</span>';
+    },
+    formatNumber: function (number, precision) {
+        if (!isFinite(number)) {
+            return number.toString();
+        }
+
+        var a = number.toFixed(precision).split('.');
+        a[0] = a[0].replace(/\d(?=(\d{3})+$)/g, '$&,');
+        return a.join('.');
+    },
+    unflattern: function (arr) {
+        var map = {};
+        var roots = [];
+        for (var i = 0; i < arr.length; i += 1) {
+            var node = arr[i];
+            node.children = [];
+            map[node.Id] = i; // use map to look-up the parents
+            if (node.ParentId !== null) {
+                arr[map[node.ParentId]].children.push(node);
+            } else {
+                roots.push(node);
+            }
+        }
+        return roots;
+    }
+}
